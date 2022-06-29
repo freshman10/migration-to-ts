52a90e285c671a138eff33c614422b08760e1d85
Alexander Marunich <myrakes4korn@gmail.com>
2022-06-29 20:14:29 +0300

    init: start project

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '2e78c10f77944c618cd50562b68377bd', // получите свой ключ https://newsapi.org/
         });
     }
 }
