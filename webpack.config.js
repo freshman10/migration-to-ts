01c805a2815ee331d213949a2fd3213f4deee8a1
Alexander Marunich <myrakes4korn@gmail.com>
2022-07-04 09:22:06 +0300

    feat: add dist folder to the project

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ const baseConfig = {
     },
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, '../dist'),
+        path: path.resolve(__dirname, './dist'),
      
     },
     plugins: [
